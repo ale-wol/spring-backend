22768a1add41ae85788348a469633aa30734a511
Alexander Wolf <alexander_wolf91@web.de>
2022-03-03 00:12:32 +0100

    first commit

---
+++ src/test/java/com/alewol/spring/backend/demo/BackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alewol.spring.backend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
